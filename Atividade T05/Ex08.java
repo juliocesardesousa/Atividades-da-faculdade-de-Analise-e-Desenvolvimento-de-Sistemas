0b3e2f3cb6b8c6197d2e017775faf28b7fde3d77
Júlio Cesar de Sousa Pimentel <132697233+juliocesardesousa@users.noreply.github.com>
2026-04-07 17:56:47 -0300

    Update Ex08.java

--- Atividade T05/Ex08.java
+++ Atividade T05/Ex08.java
@@ -1,24 +1,25 @@
 //Exercício 08:Receba o ano de nascimento de uma pessoa e informe se ela nasceu no século XX, XXI ou outro)..
 
 import java.util.Scanner;
-public class Ex08{
-    public static void main (String [] args){
-        Scanner scanner = new Scanner (System.in);
+
+public class Ex08 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         
         System.out.println("Em que ano você nasceu?");
-        int anoNascimento = scanner.nextInt();
+        int ano = scanner.nextInt();
         
-        double seculo = anoNascimento / 1000;
+        // Lógica baseada nos intervalos reais de anos
+        if (ano >= 1901 && ano <= 2000) {
+            System.out.println("Você nasceu no Século XX");
+        } 
+        else if (ano >= 2001 && ano <= 2100) {
+            System.out.println("Você nasceu no Século XXI");
+        } 
+        else {
+            System.out.println("Você nasceu em outro século (antes de 1901 ou depois de 2100)");
+        }
         
-        if (seculo <= 1 && seculo > 0)
-        {
-            System.out.println("Século XX");
-        }
-        else if (seculo <= 2 && seculo > 0)
-        {
-            System.out.println("Século XXI");
-        }
-        else    
-            System.out.println("Nasceu antes do século XX e XXI");
+        scanner.close();
     }
 }
