08c92951309bbbed0b5c808b68fc69295fb7d14a
Júlio Cesar de Sousa Pimentel <132697233+juliocesardesousa@users.noreply.github.com>
2026-04-09 18:09:36 -0300

    Add files via upload

---
+++ Atividade T06/Ex12.java
@@ -0,0 +1,30 @@
+/*Exercício 12 */
+import java.util.Scanner;
+
+public class Ex11{
+    public static void main (String [] args){
+    Scanner sc = new Scanner (System.in);
+
+    System.out.println("Digite um número inteiro");
+    int numeroTermos = sc.nextInt;
+    double somaDivisao = 0;
+    int denominador = 3;
+    int i = 0;
+    int resulatado = 0;
+
+    while(i < numeroTermos)
+    {
+        somaDivisao = somaDivisao / denominador;
+        denominador += 2;
+        if (num == 1){
+            resultado = 4 * (1 - somaDivisao);
+
+            
+        }
+        i++;
+
+
+
+    }
+ }
+}
