c978e3b9d2eef47d49b757242a12760bed746903
Júlio Cesar de Sousa Pimentel <132697233+juliocesardesousa@users.noreply.github.com>
2026-04-10 20:06:31 -0300

    Implement π approximation using Gregory-Leibniz series
    Updated the implementation to calculate π using the Gregory-Leibniz series. Fixed input handling and adjusted the formula for approximation.

--- Atividade T06/Ex12.java
+++ Atividade T06/Ex12.java
@@ -1,4 +1,13 @@
-/*Exercício 12 */
+/*Exercício 12: Calcular o valor por aproximação de π pela Série de
+Gregory-Leibniz:
+
+π = 4 x (1 - 1/3 + 1/5 - 1/7 + 1/9 - 1/11 + 1/13 ...)
+
+• Peça ao usuário o número de termos de precisão (1, 2, 3, 4 ... ) e utilize
+o while para calcular o valor aproximado de π. Na tela, compare o
+resultado encontrado com o valor dado por Math.PI do Java. Rode o
+programa várias vezes, incrementando manualmente o número de
+termos de precisão digitado (1, 2, 3, 4 ...).*/
 import java.util.Scanner;
 
 public class Ex11{
@@ -6,25 +15,22 @@ public class Ex11{
     Scanner sc = new Scanner (System.in);
 
     System.out.println("Digite um número inteiro");
-    int numeroTermos = sc.nextInt;
-    double somaDivisao = 0;
-    int denominador = 3;
+    int numeroTermos = sc.nextInt(); // Adicionado ()
+    double piAproximado = 0;
+    double denominador = 1.0;
+    double sinal = 1.0;
     int i = 0;
-    int resulatado = 0;
 
-    while(i < numeroTermos)
-    {
-        somaDivisao = somaDivisao / denominador;
+    while (i < numeroTermos) {
+        piAproximado += sinal * (1.0 / denominador);
         denominador += 2;
-        if (num == 1){
-            resultado = 4 * (1 - somaDivisao);
-
-            
-        }
+        sinal *= -1; // Inverte o sinal para o próximo termo (1, -1, 1, -1...)
         i++;
+    }
 
-
+    double resultadoFinal = 4 * piAproximado;
+    System.out.println("Pi calculado: " + resultadoFinal);
+    System.out.println("Pi do Java: " + Math.PI);
 
     }
- }
 }
