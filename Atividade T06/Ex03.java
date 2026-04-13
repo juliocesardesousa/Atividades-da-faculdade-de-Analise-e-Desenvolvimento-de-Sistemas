293ff28827057fe61dd0980a36a6a88653d45996
Júlio Cesar de Sousa Pimentel <132697233+juliocesardesousa@users.noreply.github.com>
2026-04-13 19:46:21 -0300

    Atualizar o Ex03.java

--- Atividade T06/Ex03.java
+++ Atividade T06/Ex03.java
@@ -9,7 +9,7 @@ public class Ex03 {
     public static void main (String [] args){
         Scanner scanner = new Scanner (System.in);
 
-        int i = 0;
+        int i = 1;
         double somador = 0; // somador para armazenar as temperaturas somadas.
         double temperatura = 0;
 
